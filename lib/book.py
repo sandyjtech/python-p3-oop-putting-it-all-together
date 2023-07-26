26f9fa2a894caf0632e1352041d189e33e75de8d
sandyjtech <sandyjtech@gmail.com>
2023-07-26 08:41:44 -0700

    added classes

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
+#import ipdb
 
-class Book:
-    pass
-    
+class Book:        
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count
+
+    def get_page_count(self):
+        return self._page_count
+           
+    def set_page_count(self, page_count):
+        if type(page_count) in (int, float):
+            print(f"Setting page_count to {page_count}.")
+            #use underscore to reference what is in the init
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")        
         
+    def turn_page(self):
+            print("Flipping the page...wow, you read fast!")
+            
+    page_count = property(get_page_count, set_page_count, turn_page)
+    #ipdb.set_trace()       
+    
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
+#import ipdb
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+    def get_brand(self):
+        return self._brand
+    def get_size(self):
+        return self._size
+    def set_size(self, size):
+        if type(size) in (int, float):
+            print(f"Setting size to {size}")
+        else:
+            print("size must be an integer")
+    def cobble(self):
+        #create a new instance of the object
+            self.condition = "New"          
+            print("Your shoe is as good as new!")        
+    size = property(get_size, set_size)     
+        
+
+
+  
+#ipdb.set_trace()
